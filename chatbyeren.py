b710c8388566eca2520515a65cb51c93d5a27ba2
insanolanbiri <erenakgun2007@hotmail.com>
2022-02-08 23:06:39 +0300

    perşembeeeeeeeee

--- chatbyeren.py
+++ chatbyeren.py
@@ -1,54 +1,40 @@
-import socket, select, string, sys
+import socket, select, sys
 
-#Helper function (formatting)
-def display() :
+def display():
+    #FIXME: display() fonsiyonunda saat yok
 	you="\33[33m\33[1m"+" sen: "+"\33[0m"
 	sys.stdout.write(you)
 	sys.stdout.flush()
 
 def main():
-
-    if len(sys.argv)<2:
-        host = input("bağlanılacak sunucu: ")
-    else:
-        host = sys.argv[1]
-
+    if len(sys.argv)<2: host = input("bağlanılacak sunucu: ")
+    else: host = sys.argv[1]
     port = 5001
-    
-    #asks for user name
+    msg=0
     name=input("\33[34m\33[1m kimlik oluşturuluyor:\n sizi mükemmel bir şekilde tanımlayan bir kullanıcı adı girin: \33[0m")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(2)
-    
-    # connecting host
-    try :
-        s.connect((host, port))
-    except :
+    # bağlanmayı deniyoruz
+    try: s.connect((host, port))
+    except:
         print("\33[31m\33[1m sunucuya bağlanılamadı \33[0m")
         sys.exit()
-
-    #if connected
+    # bağlanıldıysa
     s.send(bytes(name,'utf-8'))
-    display()
-    while 1:
+    while True:
         socket_list = [sys.stdin, s]
-        
-        # Get the list of sockets which are readable
-        rList, wList, error_list = select.select(socket_list , [], [])
-        
+        rList, wList, error_list = select.select(socket_list , [], [])# diğerleri bizi ilgilendirmiyor
         for sock in rList:
-            #incoming message from server
-            if sock == s:
+            if sock == s: # sunucudan gelen mesaj
                 data = sock.recv(4096)
-                if not data :
-                    print('\33[31m\33[1m \rbağlantı kesildi!\n \33[0m')
+                if not data:
+                    if msg=='exit\n': print('\33[31m\33[1m \rayrıldınız\n \33[0m')
+                    else: print('\33[31m\33[1m \rbağlantı kesildi!\n \33[0m')
                     sys.exit()
-                else :
+                else:
                     sys.stdout.write(data.decode())
                     display()
-        
-            #user entered a message
-            else :
+            else: # bizim göndereceğimiz mesaj
                 msg=sys.stdin.readline()
                 s.send(bytes(msg,'utf-8'))
                 display()
--- serverbyeren.py
+++ serverbyeren.py
@@ -1,96 +1,151 @@
 import socket, select,datetime
 
-#Function to send message to all connected clients
 def send_to_all (sock, message):
-	#Message not forwarded to server and sender itself
 	for socket in connected_list:
-		if socket != server_socket and socket != sock :
-			try :
-				socket.send(message)
-			except :
-				# if connection not available
+		if socket != server_socket and socket != sock:
+			try: socket.send(message)
+			except:
 				socket.close()
 				connected_list.remove(socket)
 
 if __name__ == "__main__":
-	name=""
-	#dictionary to store address corresponding to username
-	record={}
-	# List to keep track of socket descriptors
+	name=""# kullanıcı adını geçici olarak burada tutuyoruz
+	record={}# (ip,port):kullanici_adi eşlenikleri
 	connected_list = []
+	banned_list=[]
+	muted_list=[]
 	buffer = 4096
 	port = 5001
 
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	server_socket.bind(("localhost", port))
-	server_socket.listen(20) #listen atmost 10 connection at one time
-
-	# Add server socket to the list of readable connections
+	server_socket.bind(("0.0.0.0", port))# 0.0.0.0=tüm arayüzleri dinle
+	server_socket.listen(50)# maks. bağlantı sayısı
 	connected_list.append(server_socket)
+	print("\33[32mChat by Eren: Sunucu çalışır durumda\33[0m")
 
-	print("\33[32m \t\t\t\tChat by Eren: Sunucu çalışır durumda\33[0m")
-
-	while 1:
-        # Get the list sockets which are ready to be read through select
-		rList,wList,error_sockets = select.select(connected_list,[],[])
-
+	while True:
+		rList, wList, error_list = select.select(connected_list,[],[])# soketleri filtreliyoruz
 		for sock in rList:
-			#New connection
-			if sock == server_socket:
-				# Handle the case in which there is a new connection recieved through server_socket
+			if sock == server_socket: # yeni bir bağlantı
+				# bağlantıyı kaydediyoruz
 				sockfd, addr = server_socket.accept()
+				i, p = addr
 				name=sockfd.recv(buffer)
 				connected_list.append(sockfd)
 				record[addr]=""
-				#print "record and conn list ",record,connected_list
-                
-                #if repeated username
-				if name in record.values():
-					sockfd.send(bytes("\r\33[31m\33[1m bu kullanıcı adı alınmış\n\33[0m",'utf-8'))
+				if name in record.values(): # aynı kullanıcı adı
+					sockfd.send(bytes("\r\33[31m\33[1m bu kullanıcı adı alınmış, başka bir kullanıcı adı ile tekrar dene\n\33[0m",'utf-8'))
 					del record[addr]
 					connected_list.remove(sockfd)
 					sockfd.close()
-					continue
-				else:
-                    #add name and address
+				elif i in banned_list: # banlanmış ama bağlanmaya çalışıyor
+					sockfd.send(bytes("\r\33[31m\33[1m banlanmışsın dostum\n\33[0m",'utf-8'))
+					del record[addr]
+					connected_list.remove(sockfd)
+					sockfd.close()
+				else: # her şey yolunda
+                    # kullanıcı adını kaydediyoruz
 					record[addr]=name
-					print("%s, %s bağlandı" % addr," [",record[addr].decode(),"]")
-					sockfd.send(bytes("\33[32m\r\33[1m Chat by Eren'e hoşgeldiniz\n ==========================\n\n chatten ayrılmak istediğiniz zaman exit yazın\n not: istenmeyecek mesajlar gönderildiği taktirde banlanacaksınız.\n\n\33[0m",'utf-8'))
-					send_to_all(sockfd, bytes("\33[32m\33[1m\r "+name.decode()+" bağlandı \n\33[0m",'utf-8'))
-					continue
-
-			#Some incoming message from a client
-			else:
-				# Data from client
+					print(f"{record[(i,p)].decode()} ({i}:{p}) bağlandı")
+					sockfd.send(bytes("\33[32m\r\33\n[1m Chat by Eren'e hoşgeldiniz\n ==========================\n\n chatten ayrılmak istediğiniz zaman exit yazın\n not: istenmeyecek mesajlar gönderdiğiniz taktirde banlanacaksınız.\n\n\33[0m",'utf-8'))
+					send_to_all(sockfd, bytes(f"\33[32m\33[1m\r {record[(i,p)].decode()} bağlandı \n\33[0m",'utf-8'))
+			else: # zaten bağlı olan bir kullanıcıdan gelen mesaj
 				try:
 					data1 = sock.recv(buffer).decode()
-					data=data1.rstrip("\n")
-                    
-                    #get addr of client sending the message
+					data=data1.rstrip("\n")# sondaki \n'i atıyoruz
 					i,p=sock.getpeername()
-					if data == "exit":
-						msg="\r\33[1m"+"\33[31m "+record[(i,p)].decode()+" ayrıldı\33[0m\n"
+					if data == "exit": # çıkış mesajı
+						msg=f"\r\33[1m\33[31m {record[(i,p)].decode()} ayrıldı\33[0m\n"
 						send_to_all(sock,bytes(msg,'utf-8'))
-						print("%s, %s çevrimdışı" % (i,p)," [",record[(i,p)].decode(),"]")
+						print(f"{record[(i,p)].decode()} ({i}:{p}) ayrıldı")
 						del record[(i,p)]
+						if i in muted_list: muted_list.remove(i)
 						connected_list.remove(sock)
 						sock.close()
-						continue
-
-					else:
-						msg="\r\33[1m"+"\33[35m "+datetime.datetime.now().strftime("%H:%M: ")+record[(i,p)].decode()+": "+"\33[0m"+data+"\n"
+					elif data[:5] == "/ban ": # biri birini banlamak istiyor
+						usertoban=data[5:]
+						inv_record = [v for k, v in record.items()]
+						if bytes(usertoban,'utf-8') not in inv_record:
+							msg=f"\r\33[1m\33[31m öyle biri yok!?\33[0m\n"
+							sock.send(bytes(msg,'utf-8'))
+							continue
+						elif usertoban==record[(i,p)].decode():
+							msg=f"\r\33[1m\33[31m kendi kendini banlamak biraz saçma değil mi? banlamıyorum\33[0m\n"
+							sock.send(bytes(msg,'utf-8'))
+							continue
+						ib,pb=[key for key, value in record.items() if value == bytes(usertoban,'utf-8')][0]
+						msg=f"\r\33[1m\33[31m {usertoban} kişisi {record[(i,p)].decode()} tarafından banlandı\33[0m\n"
 						send_to_all(sock,bytes(msg,'utf-8'))
-            
-                #abrupt user exit
-				except:
+						sock.send(bytes(msg,'utf-8'))
+						print(f"{usertoban} ({ib}:{pb}) kişisi {record[(i,p)].decode()} ({i}:{p}) tarafından banlandı")
+						del record[(ib,pb)]
+						banned_list.append(ib)
+					elif data[:7] == "/unban ": # biri birini unbanlamak istiyor
+						iptounban=data[7:]
+						if iptounban not in banned_list:
+							msg=f"\r\33[1m\33[31m öyle biri yok!?\33[0m\n"
+							sock.send(bytes(msg,'utf-8'))
+							continue
+						banned_list.remove(iptounban)
+						msg=f"\r\33[1m\33[31m {iptounban} kişisi {record[(i,p)].decode()} tarafından unbanlandı\33[0m\n"
+						send_to_all(sock,bytes(msg,'utf-8'))
+						sock.send(bytes(msg,'utf-8'))
+						print(f"{iptounban} kişisi {record[(i,p)].decode()} ({i}:{p}) tarafından unbanlandı")
+					
+					elif data[:6] == "/mute ": # biri birini susturmak istiyor
+						usertomute=data[6:]
+						inv_record = [v for k, v in record.items()]
+						if bytes(usertomute,'utf-8') not in inv_record:
+							msg=f"\r\33[1m\33[31m öyle biri yok!?\33[0m\n"
+							sock.send(bytes(msg,'utf-8'))
+							continue
+						elif usertomute==record[(i,p)].decode():
+							msg=f"\r\33[1m\33[31m kendi kendini susturmak biraz saçma değil mi? susturmuyorum\33[0m\n"
+							sock.send(bytes(msg,'utf-8'))
+							continue
+						im,pm=[key for key, value in record.items() if value == bytes(usertomute,'utf-8')][0]
+						msg=f"\r\33[1m\33[31m {usertomute} kişisi {record[(i,p)].decode()} tarafından susturuldu\33[0m\n"
+						send_to_all(sock,bytes(msg,'utf-8'))
+						sock.send(bytes(msg,'utf-8'))
+						print(f"{usertomute} ({im}:{pm}) kişisi {record[(i,p)].decode()} ({i}:{p}) tarafından susturuldu")
+						muted_list.append(im)
+					elif data[:8] == "/unmute ": # biri birini sesini açmak istiyor
+						idtounmute=data[8:]
+						inv_record = [v for k, v in record.items()]
+						if idtounmute not in muted_list and bytes(idtounmute,'utf-8') not in inv_record:
+							msg=f"\r\33[1m\33[31m öyle biri yok!?\33[0m\n"
+							sock.send(bytes(msg,'utf-8'))
+							continue
+						elif idtounmute in muted_list:
+							muted_list.remove(idtounmute)
+						else:
+							inv_record_d = {v:(i,p) for ((i,p), v) in record.items()}
+							muted_list.remove(inv_record_d[bytes(idtounmute,'utf-8')][0])
+						msg=f"\r\33[1m\33[31m {idtounmute} kişisi {record[(i,p)].decode()} tarafından un-susturuldu\33[0m\n"
+						send_to_all(sock,bytes(msg,'utf-8'))
+						sock.send(bytes(msg,'utf-8'))
+						print(f"{idtounmute} kişisi {record[(i,p)].decode()} ({i}:{p}) tarafından un-susturuldu")
+
+					elif i in banned_list: # banlanmış ama hala burada
+						msg=f"\r\33[1m\33[31m banlandın dostum\33[0m\n"
+						sock.send(bytes(msg,'utf-8'))
+						sock.send(bytes(''))
+						connected_list.remove(sock)
+						sock.close()
+					elif i in muted_list: # susturulmuş ama konuşmaya çalışıyor
+						msg=f"\r\33[1m\33[31m susturuldun dostum, mesajların iletilmiyor\33[0m\n"
+						sock.send(bytes(msg,'utf-8'))
+					else: # gerçek mesaj
+						dt=datetime.datetime.now().strftime("%H:%M")
+						msg=f"\r\33[1m\33[35m {dt}: {record[(i,p)].decode()}: \33[0m{data}\n"
+						send_to_all(sock,bytes(msg,'utf-8'))
+				except OSError as e: print(f"DAHİLİ HATA: {e}")
+				except: # bişeyler olmuş, bağlantı kopması gibi
 					(i,p)=sock.getpeername()
-					send_to_all(sock, bytes("\r\33[31m \33[1m"+record[(i,p)].decode()+" bağlantısı kayboldu\33[0m\n",'utf-8'))
-					print("Hata: %s, %s çevrimdışı" % (i,p)," [",record[(i,p)].decode(),"]\n")
+					send_to_all(sock, bytes(f"\r\33[31m \33[1m{record[(i,p)].decode()} bağlantısı kayboldu\33[0m\n",'utf-8'))
+					print(f"HATA: {record[(i,p)].decode()} ({i}:{p}) çevrimdışı")
 					del record[(i,p)]
+					if i in muted_list: muted_list.remove(i)
 					connected_list.remove(sock)
 					sock.close()
-					continue
-
-	server_socket.close()
-
+	server_socket.close()# muhtemelen çalışmayacak ama dursun burada
